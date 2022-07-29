a3c78de4d76942c5b823477a0dabe318219e4b72
ell98 <82163683+ell98@users.noreply.github.com>
2022-07-29 10:36:39 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let me = {
+    name: 'ali',
+    age: 22,
+    city: 'tehran',
+}
+console.log(me)
+let users = ['ali', 'atefeh', 'david', 'zahra'];
+users[4] = 'mahrab';
+users[1] = 'ati';
+
+
+console.log(users)
